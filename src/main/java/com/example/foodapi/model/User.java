73af24eaeab5b5b356722a107025b9581bfe15ce
Paul Ogolla <paulotieno2@gmail.com>
2023-08-28 22:19:13 +0300

    create user model and add constructor

---
+++ src/main/java/com/example/foodapi/model/User.java
@@ -0,0 +1,21 @@
+package com.example.foodapi.model;
+
+public class User {
+    String firstName;
+    String lastName;
+    String emailAddress;
+    String phoneNumber;
+    String password;
+
+
+    public User(String firstName, String lastName, String emailAddress, String phoneNumber, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+
+
+}
